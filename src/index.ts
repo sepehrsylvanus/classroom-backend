c06e4804a50f48639419ea6e3a841098432c1741
sepehrsylvanus <mr.forte.world@gmail.com>
2026-02-02 12:05:30 +0300

    implement file extensions 2

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import subjectsRouter from "./routes/subjects.js";
 import cors from "cors";
 import securityMiddleware from "./middleware/security.js";
 import { toNodeHandler } from "better-auth/node";
-import { auth } from "./lib/auth";
+import { auth } from "./lib/auth.js";
 const app = express();
 const PORT = 8000;
 
