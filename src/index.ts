3fd9e11b4aee8c307f91373d03cb130943282df7
sepehrsylvanus <mr.forte.world@gmail.com>
2026-02-02 11:07:00 +0300

    implement apm with site24x7

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import AgentAPI from "apminsight";
+AgentAPI.config();
+
 import express, { Request, Response } from "express";
 import subjectsRouter from "./routes/subjects";
 import cors from "cors";
